b334a2b3ff27f2d0c50c35427186382a61674264
Hyperzsb <hyperzsb@qq.com>
2020-09-14 16:39:18 +0800

    [feat]Add destruction for type structs

---
+++ libleadao/type.cpp
@@ -0,0 +1,60 @@
+#include "type.h"
+
+SQLFeedback::~SQLFeedback() {
+    delete[]this->data;
+}
+
+Identity::~Identity() {
+    delete[]this->ip;
+    delete[]this->token;
+    delete[]this->account_name;
+}
+
+SignInFeedback::~SignInFeedback() {
+    delete[]this->token;
+    delete[]this->nickname;
+    delete[]this->description;
+}
+
+RecoverFeedback::~RecoverFeedback() {
+    delete[]this->token;
+    delete[]this->question;
+}
+
+
+Email::~Email() {
+    delete[]this->sender;
+    delete[]this->recipient;
+    delete[]this->time;
+    delete[]this->title;
+    delete[]this->body;
+}
+
+EmailFeedback::EmailFeedback() {
+    this->status = UNEXPECTED_ERROR;
+    this->email_num = 0;
+    this->email = nullptr;
+}
+
+EmailFeedback::~EmailFeedback() {
+    for (int i = 0; i < this->email_num; i++)
+        delete[]this->email[i];
+    delete[]this->email;
+}
+
+Contact::~Contact() {
+    delete[]this->contact_name;
+    delete[]this->alias;
+}
+
+ContactFeedback::ContactFeedback() {
+    this->status = UNEXPECTED_ERROR;
+    this->contact_num = 0;
+    this->contact = nullptr;
+}
+
+ContactFeedback::~ContactFeedback() {
+    for (int i = 0; i < this->contact_num; i++)
+        delete[]this->contact[i];
+    delete[]this->contact;
+}
